adce41985af7dbe471d67d0bc2ae7a00f3c10938
samuandin92 <international92>
2018-01-14 16:43:20 -0600

    Include br tags in Create and Edit Users

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -6,31 +6,31 @@
         <div class="form-group">
             {{Form::label('noabonado', 'Numero Abonado')}}
             {{Form::text('noabonado', '', ['class' => 'form-control', 'placeholder' => 'Numero de Abonado'])}}
-
+            <br>
             {{Form::label('nombre', 'Nombre')}}
             {{Form::text('nombre', '', ['class' => 'form-control', 'placeholder' => 'Nombre'])}}
-
+            <br>
             {{Form::label('apellido', 'Apellido')}}
             {{Form::text('apellido', '', ['class' => 'form-control', 'placeholder' => 'Apellido'])}}
-
+            <br>
             {{Form::label('noidentidad', 'Numero de Identidad')}}
             {{Form::text('noidentidad', '', ['class' => 'form-control', 'placeholder' => 'Numero de Identidad'])}}
-
+            <br>
             {{Form::label('telefono', 'Telefono')}}
             {{Form::text('telefono', '', ['class' => 'form-control', 'placeholder' => 'Telefono'])}}
-
+            <br>
             {{Form::label('tarifa', 'Tarifa')}}
             {{Form::text('tarifa', '', ['class' => 'form-control', 'placeholder' => 'Tarifa'])}}
-
+            <br>
             {{Form::label('tipo', 'Tipo')}}
             {{Form::text('tipo', '', ['class' => 'form-control', 'placeholder' => 'Tipo'])}}
-
+            <br>
             {{Form::label('estadomora', 'Estado de Mora')}}
             {{Form::text('estadomora', '', ['class' => 'form-control', 'placeholder' => 'Estado de Mora'])}}
-
+            <br>
             {{Form::label('tipoincidencia', 'Tipo de Incidencia')}}
             {{Form::text('tipoincidencia', '', ['class' => 'form-control', 'placeholder' => 'Tipo de Incidencia'])}}
-
+            <br>
             {{Form::label('barrio', 'Barrio')}}
             {{Form::textarea('barrio', '', ['class' => 'form-control', 'placeholder' => 'Barrio'])}}
         </div>
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -6,31 +6,31 @@
         <div class="form-group">
             {{Form::label('noabonado', 'Numero Abonado')}}
             {{Form::text('noabonado', $Users->noabonado, ['class' => 'form-control', 'placeholder' => 'Numero de Abonado'])}}
-
+            <br>
             {{Form::label('nombre', 'Nombre')}}
             {{Form::text('nombre', $Users->nombre, ['class' => 'form-control', 'placeholder' => 'Nombre'])}}
-
+            <br>
             {{Form::label('apellido', 'Apellido')}}
             {{Form::text('apellido', $Users->apellido, ['class' => 'form-control', 'placeholder' => 'Apellido'])}}
-
+            <br>
             {{Form::label('noidentidad', 'Numero de Identidad')}}
             {{Form::text('noidentidad', $Users->noidentidad, ['class' => 'form-control', 'placeholder' => 'Numero de Identidad'])}}
-
+            <br>
             {{Form::label('telefono', 'Telefono')}}
             {{Form::text('telefono', $Users->telefono, ['class' => 'form-control', 'placeholder' => 'Telefono'])}}
-
+            <br>
             {{Form::label('tarifa', 'Tarifa')}}
             {{Form::text('tarifa', $Users->tarifa, ['class' => 'form-control', 'placeholder' => 'Tarifa'])}}
-
+            <br>
             {{Form::label('tipo', 'Tipo')}}
             {{Form::text('tipo', $Users->tipo, ['class' => 'form-control', 'placeholder' => 'Tipo'])}}
-
+            <br>
             {{Form::label('estadomora', 'Estado de Mora')}}
             {{Form::text('estadomora', $Users->estadomora, ['class' => 'form-control', 'placeholder' => 'Estado de Mora'])}}
-
+            <br>
             {{Form::label('tipoincidencia', 'Tipo de Incidencia')}}
             {{Form::text('tipoincidencia', $Users->tipoincidencia, ['class' => 'form-control', 'placeholder' => 'Tipo de Incidencia'])}}
-
+            <br>
             {{Form::label('barrio', 'Barrio')}}
             {{Form::textarea('barrio', $Users->barrio, ['class' => 'form-control', 'placeholder' => 'Barrio'])}}
         </div>
